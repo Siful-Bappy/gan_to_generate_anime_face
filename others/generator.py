05f38cd3beab73bfb1ea776848f54510d3523914
Siful-Bappy <bappy.ac.bd@gmail.com>
2025-09-16 04:58:46 +0900

    generator class complete

--- others/generator.py
+++ others/generator.py
@@ -1,30 +1,3 @@
-import os
-import torch
-from torch import nn
-from torchvision.datasets import MNIST
-from torch.utils.data import DataLoader
-from torchvision import transforms
-import numpy as np
-import matplotlib.pyplot as plt
-import uuid
-
-# Configurable variables
-NUM_EPOCHS = 50
-NOISE_DIMENSION = 50
-BATCH_SIZE = 128
-TRAIN_ON_GPU = True
-UNIQUE_RUN_ID = str(uuid.uuid4())
-PRINT_STATS_AFTER_BATCH = 50
-OPTIMIZER_LR = 0.0002
-OPTIMIZER_BETAS = (0.5, 0.999)
-GENERATOR_OUTPUT_IMAGE_SHAPE = 28 * 28 * 1
-
-# Speed ups
-torch.autograd.set_detect_anomaly(False)
-torch.autograd.profiler.profile(False)
-torch.autograd.profiler.emit_nvtx(False)
-torch.backends.cudnn.benchmark = True
-
 class Generator(nn.Module):
   """
     Vanilla GAN Generator
@@ -52,5 +25,3 @@ class Generator(nn.Module):
   def forward(self, x):
     """Forward pass"""
     return self.layers(x)
-  
-
