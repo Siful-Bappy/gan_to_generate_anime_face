20bcd084a6135464baf0267d53b2268916d60803
Siful-Bappy <bappy.ac.bd@gmail.com>
2025-09-16 05:00:44 +0900

    add the helper functions

--- others/gan.py
+++ others/gan.py
@@ -71,3 +71,42 @@ class Discriminator(nn.Module):
       nn.Sigmoid()
     )
 
+  def forward(self, x):
+    """Forward pass"""
+    return self.layers(x)
+  
+  def get_device():
+    """ Retrieve device based on settings and availability. """
+    return torch.device("cuda:0" if torch.cuda.is_available() and TRAIN_ON_GPU else "cpu")
+    
+    
+def make_directory_for_run():
+  """ Make a directory for this training run. """
+  print(f'Preparing training run {UNIQUE_RUN_ID}')
+  if not os.path.exists('./runs'):
+    os.mkdir('./runs')
+  os.mkdir(f'./runs/{UNIQUE_RUN_ID}')
+
+
+def generate_image(generator, epoch = 0, batch = 0, device=get_device()):
+  """ Generate subplots with generated examples. """
+  images = []
+  noise = generate_noise(BATCH_SIZE, device=device)
+  generator.eval()
+  images = generator(noise)
+  plt.figure(figsize=(10, 10))
+  for i in range(16):
+    # Get image
+    image = images[i]
+    # Convert image back onto CPU and reshape
+    image = image.cpu().detach().numpy()
+    image = np.reshape(image, (28, 28))
+    # Plot
+    plt.subplot(4, 4, i+1)
+    plt.imshow(image, cmap='gray')
+    plt.axis('off')
+  if not os.path.exists(f'./runs/{UNIQUE_RUN_ID}/images'):
+    os.mkdir(f'./runs/{UNIQUE_RUN_ID}/images')
+  plt.savefig(f'./runs/{UNIQUE_RUN_ID}/images/epoch{epoch}_batch{batch}.jpg')
+
+
